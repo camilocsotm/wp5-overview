1a60405a616329e9e8989e04682149307281de3a
kmylo6 <kmylo6@gmail.com>
2022-03-28 17:50:17 -0300

    init commit

--- packages/data/webpack.config.js
+++ packages/data/webpack.config.js
@@ -1,36 +1,36 @@
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const path = require('path');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index',
+  entry: "./src/index",
   cache: false,
 
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
 
   optimization: {
     minimize: false,
   },
 
   output: {
-    publicPath: 'http://localhost:3004/',
+    publicPath: "http://localhost:3004/",
   },
 
   resolve: {
-    extensions: ['.jsx', '.js', '.json'],
+    extensions: [".jsx", ".js", ".json"],
   },
 
   module: {
     rules: [
       {
         test: /\.json$/,
-        loader: 'json-loader',
+        loader: "json-loader",
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
@@ -38,19 +38,19 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: 'data',
-      library: { type: 'var', name: 'data' },
-      filename: 'remoteEntry.js',
+      name: "data",
+      library: { type: "var", name: "data" },
+      filename: "remoteEntry.js",
       remotes: {},
       exposes: {
-        pokemon: './src/pokemon',
+        pokemon: "./src/pokemon",
       },
       shared: [],
     }),
   ],
 
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, "public"),
     port: 3004,
   },
 };
